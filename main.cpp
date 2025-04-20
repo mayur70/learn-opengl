3bcbba279f262d36848103642c6716b222d6cc99
mayur70 <mayurpatil70@gmail.com>
2025-04-20 14:00:19 +0530

    window creation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
 
 int main(int argc, char* argv[]) {
-	std::cout << "hello world\n";
+	if(glfwInit() != GLFW_TRUE) {
+		std::cerr << "failed to init GLFW\n";
+		std::abort();
+	}
+	
+	auto* window = glfwCreateWindow(800, 480, "learn-opengl", nullptr, nullptr);
+	if(!window) {
+		std::cerr << "failed to create window\n";
+		std::abort();
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while(!glfwWindowShouldClose(window)) {
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
